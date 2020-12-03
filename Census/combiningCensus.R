4a42c0ce016c6f9e2efc785483ff00bc9223a2c5
Noah Serr <noah.serr8@gmail.com>
2020-12-03 15:23:51 -0800

    pm25 added to income tracts.

--- Census/combiningCensus.R
+++ Census/combiningCensus.R
@@ -1,11 +1,12 @@
 #These steps will help you combine the outputs 
 #from your spatial interpolation with your income data.
 # Convert your interpolation into a raster and map it:
-r <- raster(INTERPOLATED SURFACE)
+#r <- raster(INTERPOLATED SURFACE)
 sufaceMap <- tm_shape(r) + 
-  tm_raster(n=5,palette = "viridis",
+  tm_raster(n=5,palette = "-RdBu",
             title="PM 2.5 \n(in ppm)") +
-  tm_shape(POINTS) + tm_dots(size=0.2)
+  tm_shape(pm25.mean.spdf) + tm_dots(size=0.2) +
+  tm_legend(legend.outside=TRUE)
 sufaceMap
 #If you have too many cells, 
 #you can reduce the number by aggregating values
@@ -13,4 +14,3 @@ sufaceMap
 
 #Extract average pm2.5 for each polygon
 income.tracts$Pm2.5 <- round(extract(r, income.tracts, fun = mean)[,1], 5)
-
