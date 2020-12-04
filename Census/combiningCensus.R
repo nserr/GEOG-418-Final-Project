1fb1c65503be33bddfbd20e343e87470669b5506
Noah Serr <noah.serr8@gmail.com>
2020-12-03 16:40:25 -0800

    cleaned up code.

--- Census/combiningCensus.R
+++ Census/combiningCensus.R
@@ -1,6 +1,8 @@
-#These steps will help you combine the outputs 
-#from your spatial interpolation with your income data.
-# Convert your interpolation into a raster and map it:
+###########################
+## Combine Income and PM2.5
+###########################
+
+# Convert interpolated surface into a raster and map it
 #r <- raster(INTERPOLATED SURFACE)
 sufaceMap <- tm_shape(r) + 
   tm_raster(n=5,palette = "-RdBu",
@@ -8,9 +10,6 @@ sufaceMap <- tm_shape(r) +
   tm_shape(pm25.mean.spdf) + tm_dots(size=0.2) +
   tm_legend(legend.outside=TRUE)
 sufaceMap
-#If you have too many cells, 
-#you can reduce the number by aggregating values
-#agg <- aggregate(yourRasterFromKriging, fact=??, fun=mean)
 
 #Extract average pm2.5 for each polygon
 income.tracts$Pm2.5 <- round(extract(r, income.tracts, fun = mean)[,1], 5)
